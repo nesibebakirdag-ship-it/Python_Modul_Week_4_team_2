3522961e77350cdedcc405adb60f8f2bc4bddd3b
mahocan1 <muhammetbaluk@gmail.com>
2025-09-29 12:56:08 +0200

    pyler

---
+++ teradmin.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt6 import QtWidgets, uic
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        uic.loadUi(r".\ui\tercihadmin.ui", self)  # .ui dosyasını yükler
+        self.pushButton.clicked.connect(self.butonTiklandi)
+
+    def butonTiklandi(self):
+        print("Butona tıklandı!")
+
+app = QtWidgets.QApplication(sys.argv)
+window = MyWindow()
+window.show()
+sys.exit(app.exec())  # PyQt6 tarzı
